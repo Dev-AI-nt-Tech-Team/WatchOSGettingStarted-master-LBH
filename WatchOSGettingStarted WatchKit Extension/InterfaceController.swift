dd654c65aa1a83a1a1c563e9355dd61a807bc817
lee <bg_lee@korea.ac.kr>
2021-12-13 15:09:49 +0900

    Initial Commit

---
+++ WatchOSGettingStarted WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,93 @@
+//
+//  InterfaceController.swift
+//  WatchOSGettingStarted WatchKit Extension
+//
+//  Created by Pete Gordon on 7/29/17.
+//  Copyright © 2017 UsersFirst. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    //@EnvironmentObject var workoutManager : WorkoutManager
+    
+    @IBOutlet var btnAudioRecorder: WKInterfaceButton!
+    @IBOutlet var btnAudioPlay: WKInterfaceButton!
+    @IBOutlet var displayLabel: WKInterfaceLabel!
+
+    var saveUrl: URL!
+
+    @IBAction func buttonAudioPlay() {
+        let options = [WKMediaPlayerControllerOptionsAutoplayKey : "true"]
+        
+        presentMediaPlayerController(with: saveUrl!, options: options,
+                                            completion: { didPlayToEnd, endTime, error in
+                                                if let err = error {
+                                                    print(err.localizedDescription)
+                                                }
+        })
+    }
+    
+    @IBAction func buttonAudioRecorder() {
+        displayLabel.setText("녹음 시작")
+        print("button 녹음 시작")
+        //workoutManager.averageHeartRate.formatted(
+          //  .number.precision(.fractionLength(0))
+        //) + "bpm"
+        
+   //      self.saveUrl = container?.URLByAppendingPathComponent(fileName)
+        
+        
+        let duration = TimeInterval(3600)
+        let recordOptions = [WKAudioRecorderControllerOptionsMaximumDurationKey : duration]
+        presentAudioRecorderController(withOutputURL: saveUrl!,
+                                                    preset: .wideBandSpeech,
+                                                    options: recordOptions,
+                                                    completion: { saved, error in
+                                                        
+                                                        if let err = error {
+                                                            print(err.localizedDescription)
+                                                        }
+                                                        print("녹음 종료")
+                                                        print(saved)
+                                                        if saved {
+                                                            self.btnAudioPlay.setEnabled(true)
+                                                            // Write code to execute when audio file gets saved.
+                                                            
+                                                        }
+        })
+    }
+    @IBAction func buttonDictation() {
+        displayLabel.setText("Dictation Tapped!")
+        //let phrases = ["I'm in a meeting", "I'll call you later", "Call me later"]
+        
+        self.presentTextInputController(withSuggestions: nil, allowedInputMode: .plain,
+                                                  completion: {(results) -> Void in })
+        
+    }
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+        let fileName = "audioFile.wav"
+        
+        let fileManager = FileManager.default
+        let container = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.com.petegordon.WatchOSGettingStarted")
+        saveUrl = container?.appendingPathComponent(fileName)
+        
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ WatchOSGettingStarted/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  WatchOSGettingStarted
+//
+//  Created by Pete Gordon on 7/29/17.
+//  Copyright © 2017 UsersFirst. All rights reserved.
+//
+
+import UIKit
+import Speech
+
+class ViewController: UIViewController, SFSpeechRecognizerDelegate  {
+
+    @IBOutlet weak var btnStartStop: UIButton!
+    @IBOutlet weak var BPM: UILabel!
+    @IBOutlet weak var textOutput: UILabel!
+     
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale.init(identifier: "ko-KR"))
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        recordAndRecognizeSpeech()
+    }
+    
+    func recordAndRecognizeSpeech(){
+        let node = audioEngine.inputNode
+
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat){ buffer, _ in self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do{
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ko-KR")) else {
+            //A recognizer is not supported for the current locale
+            return
+        }
+        if(!myRecognizer.isAvailable){
+            //A recognizer is not available right now.
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: {result, error in
+            if let result = result {
+                
+                let bestString = result.bestTranscription.formattedString
+                self.textOutput.text = bestString
+                
+                //if timespan exists add as event on calendar
+                /*
+                 // Create an Event Store instance
+                 let eventStore = EKEventStore();
+                 
+                 // Use Event Store to create a new calendar instance
+                 if let calendarForEvent = eventStore.calendarWithIdentifier(self.calendar.calendarIdentifier)
+                 {
+                 let newEvent = EKEvent(eventStore: eventStore)
+                 
+                 newEvent.calendar = calendarForEvent
+                 newEvent.title = self.eventNameTextField.text ?? "Some Event Name"
+                 newEvent.startDate = self.eventStartDatePicker.date
+                 newEvent.endDate = self.eventEndDatePicker.date
+                 
+                 // Save the calendar using the Event Store instance
+                 
+                 do {
+                 try eventStore.saveEvent(newEvent, span: .ThisEvent, commit: true)
+                 delegate?.eventDidAdd()
+                 
+                 self.dismissViewControllerAnimated(true, completion: nil)
+                 } catch {
+                 let alert = UIAlertController(title: "Event could not save", message: (error as NSError).localizedDescription, preferredStyle: .Alert)
+                 let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                 alert.addAction(OKAction)
+                 
+                 self.presentViewController(alert, animated: true, completion: nil)
+                 }
+                 }
+                     */
+                
+            }else if let error = error {
+                print(error)
+            }
+        })
+    }
+
+}
+
